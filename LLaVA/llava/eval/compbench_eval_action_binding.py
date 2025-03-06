d41a30143b44fca5cd1bcef24b8935acc21f154c
Kaiyue Sun <94892075+KaiyueSun98@users.noreply.github.com>
2025-03-06 17:00:33 +0800

    Update compbench_eval_action_binding.py

--- LLaVA/llava/eval/compbench_eval_action_binding.py
+++ LLaVA/llava/eval/compbench_eval_action_binding.py
@@ -253,7 +253,7 @@ def eval_model(args):
             Q1 = "The provided image arranges key frames from an AI generated video in a grid layout.  Describe the video, highlight all the characters and objects that appear throughout the frames and indicate how they act."
             qs1 = Q1
             image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
-            qs1 = DEFAULT_IMAGE_TOKEN + "\n" + qs
+            qs1 = DEFAULT_IMAGE_TOKEN + "\n" + qs1
             
             conv_mode = "chatml_direct"
 
@@ -483,7 +483,7 @@ def model_score(csv_path):
             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", type=str, default="llava-v1.6-34b")
+    parser.add_argument("--model-path", type=str, default="liuhaotian/llava-v1.6-34b")
     parser.add_argument("--model-base", type=str, default=None)
     parser.add_argument("--conv-mode", type=str, default=None)
     parser.add_argument("--sep", type=str, default=",")

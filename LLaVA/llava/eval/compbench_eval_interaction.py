63dbc04104217934a6c7c91622ab43f8bf169fb1
Kaiyue Sun <94892075+KaiyueSun98@users.noreply.github.com>
2025-03-06 17:03:07 +0800

    Update compbench_eval_interaction.py

--- LLaVA/llava/eval/compbench_eval_interaction.py
+++ LLaVA/llava/eval/compbench_eval_interaction.py
@@ -249,7 +249,7 @@ def eval_model(args):
             qs1 = Q1
             image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
             
-            qs1 = DEFAULT_IMAGE_TOKEN + "\n" + qs
+            qs1 = DEFAULT_IMAGE_TOKEN + "\n" + qs1
             
             conv_mode = "chatml_direct"
 
@@ -485,7 +485,7 @@ def model_score(csv_path):
 if __name__ == "__main__":
 
         parser = argparse.ArgumentParser()
-        parser.add_argument("--model-path", type=str, default="llava-v1.6-34b")
+        parser.add_argument("--model-path", type=str, default="liuhaotian/llava-v1.6-34b")
         parser.add_argument("--model-base", type=str, default=None)
         parser.add_argument("--conv-mode", type=str, default=None)
         parser.add_argument("--sep", type=str, default=",")
